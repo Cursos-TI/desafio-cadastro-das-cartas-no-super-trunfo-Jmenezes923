51d8943bd1dd556afa945f5402ca952e3f58ebf4
Jmenezes923 <joaomarcos.menezes.923@gmail.com>
2025-03-14 01:08:30 +0000

    atualizacao

--- DesafioCartasSuperTrunfo.c
+++ DesafioCartasSuperTrunfo.c
@@ -2,15 +2,17 @@
 
 int main(){
 
-    printf("Desafio Super Trunfo!!\n");
-
     char carta = 1;
     char estado [20] = "A";
     char código [20] = "A01";
     char cidade [20] = "São Paulo";
-    int população [20]= ('-11.450.000,00')- *("onze milhões e quatrocentos mil");
+    int população [20]= ('-11.450.000,00');
     float area [10] = ('1.521 km²');
-    float PIB [20]= ('R$ 2.719.751.231,00');
+    float PIB [20] = ('R$ 2.719.751.231,00');
+    int numero_de_pontos_turisticos = 65;
+
+
+    printf("digite a carta");
 
 
 
