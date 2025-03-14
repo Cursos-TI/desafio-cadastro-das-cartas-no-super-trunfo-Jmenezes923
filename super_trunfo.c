80697186ec2f3031277d6f14163eefcd0a3ace01
Jmenezes923 <joaomarcos.menezes.923@gmail.com>
2025-03-14 03:06:01 +0000

    novo commit

---
+++ super_trunfo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    char estado1 [20] = "A";
+    char código1 [20] = "A01";
+    char cidade1 [20] = "São Paulo";
+    int população1 [20]= '-11.450.000,00';
+    float area1 [10] = '1.521 km²';
+    float PIB1 [20] = 'R$ 2.719.751.231,00';
+    int numero_de_pontos_turisticos1 = 65;
+
+    printf("Estado: %s \n " , estado1);
+
+
+
+
+
+
+}
