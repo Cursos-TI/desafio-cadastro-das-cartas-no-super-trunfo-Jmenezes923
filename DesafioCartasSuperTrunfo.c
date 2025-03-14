65fde54c6f106815efa2639813b29ab56ab23be6
Jmenezes923 <joaomarcos.menezes.923@gmail.com>
2025-03-14 00:54:19 +0000

    novo commit

--- DesafioCartasSuperTrunfo.c
+++ DesafioCartasSuperTrunfo.c
@@ -3,4 +3,25 @@
 int main(){
 
     printf("Desafio Super Trunfo!!\n");
+
+    char carta = 1;
+    char estado [20] = "A";
+    char código [20] = "A01";
+    char cidade [20] = "São Paulo";
+    int população [20]= ('-11.450.000,00')- *("onze milhões e quatrocentos mil");
+    float area [10] = ('1.521 km²');
+    float PIB [20]= ('R$ 2.719.751.231,00');
+
+
+
+    
+
+
+
+
+
+
+
+
+
 }
