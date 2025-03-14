aa5ca6829349b948ffc5f99568b62107c8db02a9
Jmenezes923 <joaomarcos.menezes.923@gmail.com>
2025-03-14 01:45:48 +0000

    comit novo

--- DesafioCartasSuperTrunfo.c
+++ DesafioCartasSuperTrunfo.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main(){
 
-    char carta = 1;
+    
     char estado [20] = "A";
     char código [20] = "A01";
     char cidade [20] = "São Paulo";
@@ -11,19 +11,31 @@ int main(){
     float PIB [20] = ('R$ 2.719.751.231,00');
     int numero_de_pontos_turisticos = 65;
 
+    printf("estado: A \n");
+    scanf("%d , &estado \n");
 
-    printf("digite a carta");
+    printf("código: A01 \n");
+    scanf("%d , &código \n");
 
+    printf("cidade: São Paulo");
+    scanf("%d , &cidade \n");
+
+    printf("população: 11.450.000,00");
+    scanf("%s , &população \n");
+
+    printf("area: 1.521 km² ");
+    scanf("%f , &area \n");
+
+    printf("PIB: R$ 2.719.751.231,00 ");
+    scanf("%f , &PIB \n");
+
+    printf("numero_de_pontos_turisticos: 65");
+    scanf("%s , &numero_de_pontos_turisticos");
+
+    return 0;
 
 
     
-
-
-
-
-
-
-
 
 
 }
